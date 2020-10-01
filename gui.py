6f208c68c2f19a5baff5eaa454819e0d8d8a87e4
maddypie <tsadarsh070707@gmail.com>
2020-10-01 19:03:21 +0530

    Code clean-up

--- gui.py
+++ gui.py
@@ -52,11 +52,8 @@ class GUI(Tk):
                               background='SeaGreen2',
                               foreground='green4')
         self.styler.configure("EqualButton2.TButton",
-<<<<<<< HEAD
                               relief='flat',
-=======
                               relief='falt',
->>>>>>> 30b548400aef650592c82297aa6da1d3e89049bb
                               background='firebrick1',
                               foreground='green4')
         self.styler.configure("Outliner.TFrame",
@@ -107,7 +104,6 @@ class GUI(Tk):
 
     def create_advanced_buttons(self):
         keypad = ttk.Frame(self.mainframe2)
-<<<<<<< HEAD
         button_objects = {
                 button: ttk.Button(
                         master=keypad,
@@ -117,11 +113,9 @@ class GUI(Tk):
                         )
                 for button in self._layout
                 }
-=======
         button_objects = {button: ttk.Button(keypad, text=button,
             command=lambda button=self._layout[button]: self._button_invoke(button))
             for button in self._layout}
->>>>>>> 30b548400aef650592c82297aa6da1d3e89049bb
         button_objects['=']['style'] = 'EqualButton2.TButton'
 
         keypad.grid()
