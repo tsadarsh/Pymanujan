ba5cd2c6d75bc2653cf1aee53d75fa0dfb4a1390
maddypie <tsadarsh070707@gmail.com>
2020-10-03 22:25:42 +0530

    Added test cases to core_logic.py

--- test/test_core_logic.py
+++ test/test_core_logic.py
@@ -4,30 +4,34 @@ from core_logic import Calculate
 
 
 class CoreLogicTests(unittest.TestCase):
-    def go_for_testing(self, test_case_input: str):
-        test_case_input = list(test_case_input)
+    def go_for_testing(self, test_case_input: list):
         calculate_instance = Calculate(test_case_input)
         result = calculate_instance.calculate()
         return result
 
     def test_calculate_works(self):
-        test_case_input = "1+2+3"
+        test_case_input = ['1', '+', '3']
         result = self.go_for_testing(test_case_input)
 
-        self.assertEqual(result, "6.0")
+        self.assertEqual(result, "4.0")
 
     def test_left_paren_as_first_input(self):
-        test_case_input = "(12)"
+        test_case_input = ['(', '12', ')']
         result = self.go_for_testing(test_case_input)
 
         self.assertEqual(result, "12")
 
     def test_parenthesis_precedence(self):
-        test_case_input = "(3+2)*2"
+        test_case_input = ['(', '3', '+', '2', ')', '*', '2']
         result = self.go_for_testing(test_case_input)
 
         self.assertEqual(result, "10.0")
 
+    def test_bracket_balencer(self):
+        test_case_input = ['(', '3', '*', '(', '4', '+', '1']
+        result = self.go_for_testing(test_case_input)
+
+        self.assertEqual(result, "15.0")
 
 if __name__ == "__main__":
     unittest.main()
