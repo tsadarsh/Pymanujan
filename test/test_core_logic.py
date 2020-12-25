40b46fb26395953270789f2a8e9810f1e45b8101
maddypie <tsadarsh070707@gmail.com>
2020-12-25 10:40:34 +0530

    changes to update radians to degrees

--- test/test_core_logic.py
+++ test/test_core_logic.py
@@ -48,26 +48,26 @@ class CoreLogicTests(unittest.TestCase):
     def test_trigonometric_func(self):
         test_case_input_1 = ['sin', '90']
         test_case_input_2 = ['cos', '90']
-        test_case_input_3 = ['tan', '90']
+        test_case_input_3 = ['tan', '45']
         result_1 = self.go_for_testing(test_case_input_1)
         result_2 = self.go_for_testing(test_case_input_2)
         result_3 = self.go_for_testing(test_case_input_3)
 
-        self.assertAlmostEqual(float(result_1), 0.8939966636005579, places=3)
-        self.assertAlmostEqual(float(result_2), -0.4480736161291701, places=3)
-        self.assertAlmostEqual(float(result_3), -1.995200412208242, places=3)
+        self.assertAlmostEqual(float(result_1), 1.0, places=3)
+        self.assertAlmostEqual(float(result_2), 0.0, places=3)
+        self.assertAlmostEqual(float(result_3), 1.0, places=3)
 
     def test_arctrigonometric_func(self):
-        test_case_input_1 = ['asin', '0.89']
-        test_case_input_2 = ['acos', '-0.44']
-        test_case_input_3 = ['atan', '-1.99']
+        test_case_input_1 = ['asin', '1.0']
+        test_case_input_2 = ['acos', '0.0']
+        test_case_input_3 = ['atan', '1.0']
         result_1 = self.go_for_testing(test_case_input_1)
         result_2 = self.go_for_testing(test_case_input_2)
         result_3 = self.go_for_testing(test_case_input_3)
 
-        self.assertAlmostEqual(float(result_1), 1.0973451695228305, places=3)
-        self.assertAlmostEqual(float(result_2), 2.02639500019072, places=3)
-        self.assertAlmostEqual(float(result_3), -1.1051406883644943, places=3)
+        self.assertAlmostEqual(float(result_1), 90.0, places=3)
+        self.assertAlmostEqual(float(result_2), 90.0, places=3)
+        self.assertAlmostEqual(float(result_3), 45.0, places=3)
 
     def test_factorial(self):
         test_case_input = ['3', '!']
